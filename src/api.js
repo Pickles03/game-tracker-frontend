d8ad5b81ea111397ae6f9799dc1c8ff66bf67ac6
Pickles03 <elizaveta.kris@gmail.com>
2025-07-26 20:41:17 +0200

    changed url to render url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://game-tracker-backend-4uwd.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
